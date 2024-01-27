d4ff9caf271ec127fc3f51c18e9bf1d5020b1a09
ahkubek <120653318+ahkubek@users.noreply.github.com>
2024-01-27 07:20:14 +0300

    zad

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-      //  System.out.println("Дадашка выдающийся программист  java");
-        for (int i = 1000; i >0 ; i--) {
-            if (i%2==0)
-            System.out.println(i);
-            
+task1();
+
+
+    }
+        static void task1() {
+            for (int i = 1000; i > 0; i--) {
+                if (i % 2 == 0)
+                    System.out.println(i);
+
+            }
         }
     }
-}
